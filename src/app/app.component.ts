09f604c09a88a5eba67b54c81aa90be2efa5ff95
Febys Hoxha <51793241+febys@users.noreply.github.com>
2021-03-12 16:35:02 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mangerOrDirector';
+    numberOfClicks = 0;
+    approved:boolean = false;
+    notapproved:boolean = false;
+    noaction:boolean = false
+  count:any = [];
+    onClickM(event) {
+    this.numberOfClicks++;
+      if(this.numberOfClicks == 1){
+        this.approved = true;
+        this.notapproved = false;
+        this.noaction = false
+      }
+    else if(this.numberOfClicks == 2){
+        this.notapproved = true;  
+        this.approved = false;
+        this.noaction = false
+      }
+      else if(this.numberOfClicks > 2){
+        this.numberOfClicks = 0;
+        this.noaction = true
+        this.approved = false;
+        this.notapproved = false;
+      }
+   
+      console.log('number of clicks:', this.numberOfClicks);
+   }
+   onClickD(event) {
+    this.numberOfClicks++;
+    if(this.numberOfClicks == 1){
+      this.approved = true;
+      this.notapproved = false;
+      this.noaction = false
+    }
+  else if(this.numberOfClicks == 2){
+      this.notapproved = true;  
+      this.approved = false;
+      this.noaction = false
+    }
+    else if(this.numberOfClicks > 2){
+      this.numberOfClicks = 0;
+      this.noaction = true
+      this.approved = false;
+      this.notapproved = false;
+    }
+ 
+    console.log('number of clicks:', this.numberOfClicks);
+ }
+  
 }
